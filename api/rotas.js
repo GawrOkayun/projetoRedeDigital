b4409a1d42b7ee6be993581564f38e8376d5afb8
unknown <diegoreis.dvlopr@gmail.com>
2023-03-19 23:49:48 -0300

    atualizando rotas do api

--- api/rotas.js
+++ api/rotas.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { causaGet, causaPost, causaPut, causaDelete,organizacaoGet, organizacaoPost, organizacaoPut, organizacaoDelete } from './controller/querys.js'
+import { causaGet, causaPost, causaPut, causaDelete,organizacaoGet, organizacaoPost, organizacaoPut, organizacaoDelete, usuarioGet, usuarioPost, usuarioPut, usuarioDelete } from './controller/querys.js'
 
 const router = express.Router()
 
@@ -11,6 +11,10 @@ router.get('/organizacao', organizacaoGet)
 router.post('/organizacao', organizacaoPost)
 router.put('/organizacao/:id', organizacaoPut)
 router.delete('/organizacao/:id', organizacaoDelete)
+router.get('/usuario', usuarioGet)
+router.post('/usuario', usuarioPost)
+router.put('/usuario/:id', usuarioPut)
+router.delete('/usuario/:id', usuarioDelete)
 
 
 export default router;
